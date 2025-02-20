0d0a7a186d83fd88fa267bdfb5fd2fcdfb4e9f0a
giorgos ntemiris <ntemirisgiorgos3@gmail.com>
2025-02-20 23:01:49 +0100

    Refactor command parsing to use an enum for commands and improve parsing logic

--- workshop1-nom/src/lib.rs
+++ workshop1-nom/src/lib.rs
@@ -1,30 +1,103 @@
 use nom::{
-    bytes::{complete::take_until, tag},
-    character::complete::{alphanumeric1, char, space0},
-    sequence::{delimited, separated_pair},
+    branch::alt,
+    bytes::complete::{tag, take_until},
+    character::complete::{line_ending, space0, space1},
+    combinator::{rest},
     IResult, Parser,
 };
 
-fn parse_command(input: &str) -> IResult<&str, (&str, Option<(&str, &str)>)> {
-    let (input, command) = alphanumeric1(input)?;
+#[derive(Debug, PartialEq)]
+pub enum Command {
+    Publish { topic: String, message: String },
+    Subscribe { topic: String, mode: String },
+    Unsubscribe { topic: String },
+    Info,
+    Ping,
+    Pong,
+    Connect,
+    Disconnect,
+}
+
+pub fn parse_command(input: &str) -> IResult<&str, Command, nom::error::Error<&str>> {
+    let (input, cmd) = take_until("\r\n")(input)?;
+    let (input, _) = line_ending(input)?;
+
+    match cmd {
+        "CONNECT" => IResult::Ok((input, Command::Connect)),
+        "INFO" => IResult::Ok((input, Command::Info)),
+        "PING" => IResult::Ok((input, Command::Ping)),
+        "PONG" => IResult::Ok((input, Command::Pong)),
+        "DISCONNECT" => IResult::Ok((input, Command::Disconnect)),
+        _ => {
+            let known = alt([
+                tag::<&str, &str, nom::error::Error<&str>>("PUBLISH"),
+                tag("SUBSCRIBE"),
+                tag("UNSUBSCRIBE"),
+            ])
+            .parse(cmd);
+
+            if known.is_ok() {
+                let w = match known.unwrap().1 {
+                    "PUBLISH" => parse_publish(cmd),
+                    "SUBSCRIBE" => parse_subscribe(cmd),
+                    "UNSUBSCRIBE" => parse_unsubscribe(cmd),
+                    _ => unreachable!(),
+                };
+                if w.is_ok() {
+                    return w;
+                }
+            }
+            IResult::Err(nom::Err::Error(nom::error::Error::new(
+                input,
+                nom::error::ErrorKind::Tag,
+            )))
+        }
+    }
+}
+
+fn parse_publish(input: &str) -> IResult<&str, Command, nom::error::Error<&str>> {
+    let (input, _) = tag("PUBLISH")(input)?;
     let (input, _) = space0(input)?;
-    let (input, args) = match command {
-        "INFO" => {
-            let (input, _) = tag("\r\n").parse(input)?;
-            (input, None)
-        }
-        "PUB" => {
-            let mut p = delimited(char(' '), take_until(" "), take_until("\r\n"));
-            let parsed = p.parse(input)?;
-            (input, Some(parsed))
-        }
-        "SUB" => {
-            let (input, topic) = take_until("\r\n")(input)?;
-            (input, Some((topic, "")))
-        }
-        _ => (input, None),
-    };
-    Ok((input, (command, args)))
+    let (input, topic) = take_until(" ")(input)?;
+    let (input, _) = space1(input)?;
+    let (leftover, input) = rest(input)?;
+
+    Ok((
+        leftover,
+        Command::Publish {
+            topic: topic.to_string(),
+            message: input.to_string(),
+        },
+    ))
+}
+
+fn parse_subscribe(input: &str) -> IResult<&str, Command, nom::error::Error<&str>> {
+    let (input, _) = tag("SUBSCRIBE")(input)?;
+    let (input, _) = space0(input)?;
+    let (input, topic) = take_until(" ")(input)?;
+    let (input, _) = space1(input)?;
+    let (leftover, input) = rest(input)?;
+
+    Ok((
+        leftover,
+        Command::Subscribe {
+            topic: topic.to_string(),
+            mode: input.to_string(),
+        },
+    ))
+}
+
+fn parse_unsubscribe(input: &str) -> IResult<&str, Command, nom::error::Error<&str>> {
+    let (input, _) = tag("UNSUBSCRIBE")(input)?;
+    let (input, _) = space0(input)?;
+    let (leftover, input) = rest(input)?;
+
+    Ok((
+        leftover,
+        Command::Unsubscribe {
+            topic: input.to_string(),
+        },
+    ))
 }
 
 #[cfg(test)]
@@ -32,24 +105,55 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_sub_cmd() {
+    fn test_parse_no_args_cmds() {
+        assert_eq!(parse_command("CONNECT\r\n"), Ok(("", Command::Connect)));
+        assert_eq!(parse_command("INFO\r\n"), Ok(("", Command::Info)));
+        assert_eq!(parse_command("PING\r\n"), Ok(("", Command::Ping)));
+        assert_eq!(parse_command("PONG\r\n"), Ok(("", Command::Pong)));
         assert_eq!(
-            parse_command("SUB foo\r\n"),
-            Ok(("\r\n", ("SUB", Some(("foo", "")))))
+            parse_command("DISCONNECT\r\n"),
+            Ok(("", Command::Disconnect))
         );
     }
 
     #[test]
-    fn test_pub_cmd() {
+    fn test_parse_publish() {
         assert_eq!(
-            parse_command("PUB foo bar\r\n"),
-            Ok(("\r\n", ("PUB", Some(("foo", "bar")))))
+            parse_command("PUBLISH topic message\r\n"),
+            Ok((
+                "",
+                Command::Publish {
+                    topic: "topic".to_string(),
+                    message: "message".to_string(),
+                }
+            ))
         );
     }
 
     #[test]
-    fn test_info_cmd() {
-        assert_eq!(parse_command("INFO\r\n"), Ok(("", ("INFO", None))));
+    fn test_parse_subscribe() {
+        assert_eq!(
+            parse_command("SUBSCRIBE topic mode\r\n"),
+            Ok((
+                "",
+                Command::Subscribe {
+                    topic: "topic".to_string(),
+                    mode: "mode".to_string(),
+                }
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_unsubscribe() {
+        assert_eq!(
+            parse_command("UNSUBSCRIBE topic\r\n"),
+            Ok((
+                "",
+                Command::Unsubscribe {
+                    topic: "topic".to_string()
+                }
+            ))
+        );
     }
 }
- 
